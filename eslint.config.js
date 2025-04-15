6495cb8cc64f676f6f93ca262d79d6429e5b49a4
Felipe Barso <77860630+aprendendofelipe@users.noreply.github.com>
2025-04-15 18:21:22 -0300

    refactor(eslint): simplify config by importing directly from package

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,1 @@
-import defaultConfig from '@tabnews/config/eslint';
-
-const config = [
-  ...defaultConfig,
-  {
-    rules: {
-      'import/no-useless-path-segments': ['error', {}],
-    },
-  },
-];
-
-export default config;
+export { default } from '@tabnews/config/eslint';
